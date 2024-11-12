afc4bec34043d26a894679238cd7a40cf07855ca
Sam Newell <samnewell91@gmail.com>
2024-11-12 17:45:52 +0000

    initial commit

---
+++ src/lib/recipes.js
@@ -0,0 +1,11 @@
+export async function fetchRecipes() {
+  try {
+    const response = await fetch("https://dummyjson.com/recipes");
+    if (!response.ok) throw new Error("Failed to fetch recipes");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
